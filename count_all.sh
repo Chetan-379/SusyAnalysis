f03a8818fc50d3d330da76e20ef042c66758a4c8
Chetan <chetan.agrawal@students.iiserpune.ac.in>
2024-08-24 12:36:31 +0200

    shell script to count no. for all samples

---
+++ count_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+./analyzeTProxytBSM runFiles/runList_TTGJets_Tune_Summer20UL18.txt Summer20UL18_TTGJets_Tune.root 2018 TTG LL MVA
+./analyzeTProxytBSM runFiles/runList_ZNuNuGJets_MonoPhoton_Summer20UL18.txt Summer20UL18_ZNuNuGJets_MonoPhoton.root 2018 ZG LL MVA
+./analyzeTProxytBSM runFiles/runList_WJetsToLNu_HT_Summer20UL18.txt Summer20UL18_WJetsToLNu_HT.root 2018 WJets LL MVA
+./analyzeTProxytBSM runFiles/runList_QCD_HT_Summer20UL18.txt Summer20UL18_QCD_HT.root 2018 QCD LL MVA
+./analyzeTProxytBSM runFiles/runList_GJets_DR-0p4_HT_Summer20UL18.txt Summer20UL18_GJets_DR-0p4_HT.root 2018 GJets LL MVA
+
+./analyzeTProxytBSM WGJets_40to130.txt Summer20UL18_WGJets_MonoPhoton_40to130.root 2018 WGJets_MonoPhoton_PtG-40to130UL LL MVA
+./analyzeTProxytBSM WGJets_130.txt Summer20UL18_WGJets_MonoPhoton_130.root 2018 WGJets_MonoPhoton_PtG-130UL LL MVA
+rm Summer20UL18_WGJets_MonoPhoton.root
+hadd Summer20UL18_WGJets_MonoPhoton.root Summer20UL18_WGJets_MonoPhoton_40to130.root Summer20UL18_WGJets_MonoPhoton_130.root
+
+./analyzeTProxytBSM runFiles/runList_TTJets_HT_Summer20UL18.txt Summer20UL18_TTJets_HT.root 2018 TTJets LL MVA
+./analyzeTProxytBSM runFiles/runList_TTJets_Leptons_Summer20UL18.txt Summer20UL18_TTJets_Leptons.root2018 TTJets_Leptons LL MVA
+rm Summer20UL18_TTJets.root
+hadd Summer20UL18_TTJets.root Summer20UL18_TTJets_HT.root Summer20UL18_TTJets_Leptons.root
