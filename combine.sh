4ab7c5de9dd7f385e41913d87c9a43ecee005fea
Chetan <chetan.agrawal@students.iiserpune.ac.in>
2024-09-20 09:42:08 +0200

    bash script to combine samples for Lost e bkg

---
+++ combine.sh
@@ -0,0 +1,3 @@
+hadd -f TTGJets_process_combined_Summer20UL18.root Summer20UL18_TTJets.root Summer20UL18_TTGJets_Tune.root
+hadd -f WGJets_process_combined_Summer20UL18.root Summer20UL18_WJetsToLNu_HT.root Summer20UL18_WGJets_MonoPhoton.root
+hadd -f All_combined_Summer20UL18.root Summer20UL18_TTJets.root Summer20UL18_TTGJets_Tune.root Summer20UL18_WJetsToLNu_HT.root Summer20UL18_WGJets_MonoPhoton.root
