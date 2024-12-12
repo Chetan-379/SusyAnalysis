b92d0ac46b57e2ca45b4aeac52cb80c55313eba4
Chetan <chetan.agrawal@students.iiserpune.ac.in>
2024-12-12 06:26:43 +0100

    script to copy plots to webpage

---
+++ plottingScripts/copyplots.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#cd /afs/cern.ch/user/c/cagrawal/public/MyAnalysis/CMSSW_14_0_2/src/Alpana_FR_machinery/plottingScripts/Results/SRvsCR_KinemVar
+cd /afs/cern.ch/user/c/cagrawal/public/MyAnalysis/CMSSW_14_0_2/src/Alpana_FR_machinery/plottingScripts/Results/Validation_MC/SRbins/Bins_inPhopT_Qmulti
+
+# #for Individual samples
+# cp *CR_TTJets_2016preVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016preVFP_/TT_
+# cp *CR_TTGJets_2016preVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016preVFP_/TTG_
+# cp *CR_WJets_2016preVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016preVFP_/WLNu_/
+# cp *CR_WGJets_2016preVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016preVFP_/WG_/
+
+# cp *CR_TTJets_2016postVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016postVFP_/TT_
+# cp *CR_TTGJets_2016postVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016postVFP_/TTG_
+# cp *CR_WJets_2016postVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016postVFP_/WLNu_/
+# cp *CR_WGJets_2016postVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016postVFP_/WG_/
+
+# cp *CR_TTJets_2017*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2017_/TT_
+# cp *CR_TTGJets_2017*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2017_/TTG_
+# cp *CR_WJets_2017*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2017_/WLNu_/
+# cp *CR_WGJets_2017*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2017_/WG_/
+
+# cp *CR_TTJets_2018*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2018_/TT_
+# cp *CR_TTGJets_2018*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2018_/TTG_
+# cp *CR_WJets_2018*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2018_/WLNu_/
+# cp *CR_WGJets_2018*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2018_/WG_/
+
+# #for all_samples_combined of individual year
+# cp *CR_W+TTBar_2016preVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016preVFP_/TT+W_
+# cp *CR_W+TTBar_2016postVFP*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2016postVFP_/TT+W_
+# cp *CR_W+TTBar_2017*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2017_/TT+W_
+# cp *CR_W+TTBar_2018*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/2018_/TT+W_
+# cp *CR_W+TTBar_FullRun2*.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/SR_CR/FullRun2_/TT+W_
+
+
+#FOR CLOSURE TEST
+cp *qmulti_TTJets_2016preVFP* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016preVFP_/TT_
+cp *qmulti_TTJets_2016postVFP* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016postVFP_/TT_
+cp *qmulti_TTJets_2017* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2017_/TT_
+cp *qmulti_TTJets_2018* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2018_/TT_
+cp *qmulti_Run2_TTJets* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/FullRun2_/TT_
+
+cp *qmulti_TTGJets_2016preVFP.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016preVFP_/TTG_
+cp *qmulti_TTGJets_2016postVFP.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016postVFP_/TTG_
+cp *qmulti_TTGJets_2017.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2017_/TTG_
+cp *qmulti_TTGJets_2018.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2018_/TTG_
+cp *qmulti_Run2_TTGJets.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/FullRun2_/TTG_
+
+cp *qmulti_WJets_2016preVFP* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016preVFP_/WLNu_
+cp *qmulti_WJets_2016postVFP* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016postVFP_/WLNu_
+cp *qmulti_WJets_2017* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2017_/WLNu_
+cp *qmulti_WJets_2018* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2018_/WLNu_
+cp *qmulti_Run2_WJets* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/FullRun2_/WLNu_
+
+cp *qmulti_WGJets_2016preVFP.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016preVFP_/WG_
+cp *qmulti_WGJets_2016postVFP.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016postVFP_/WG_
+cp *qmulti_WGJets_2017.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2017_/WG_
+cp *qmulti_WGJets_2018.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2018_/WG_
+cp *qmulti_Run2_WGJets.png /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/FullRun2_/WG_
+
+cp *qmulti_W+TTBar_2016preVFP* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016preVFP_/TT+W_
+cp *qmulti_W+TTBar_2016postVFP* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2016postVFP_/TT+W_
+cp *qmulti_W+TTBar_2017* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2017_/TT+W_
+cp *qmulti_W+TTBar_2018* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/2018_/TT+W_
+cp *W+TTBar_FullRun2* /eos/home-c/cagrawal/www/folder/FR_estimation_unskimmed/Closure_test/FullRun2_/TT+W_
